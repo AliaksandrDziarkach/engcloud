713a7939a9a9adb597135e6b83de9b1672f6f8e0
John Belamaric <jbelamaric@infoblox.com>
2016-03-14 19:31:30 +0000

    Refactor config script and add standalone
     - Added a script to configure a standalone GM.
     - Refactored the config scripts to be shared between the standalone
    and HA configurations.
     - Add README to show usage
     - Add logs from example runs

--- grid-templates/config-gm-ha.sh
+++ grid-templates/config-gm-ha.sh
@@ -7,123 +7,7 @@ if [[ -z "$OS_USERNAME" ]]; then
 	exit 1
 fi
 
-
-## FUNCTIONS
-
-function port_first_fixed_ip() {
-	neutron port-show -c fixed_ips -f value $1 | sed -e 's/.*ip_address": "\([0-9\.]*\)".*/\1/'
-}
-
-function port_gw() {
-	subnet_id=$(neutron port-show -c fixed_ips -f value $1 | sed -e 's/.*"subnet_id": "\([-a-z0-9]*\)", .*/\1/')
-	neutron subnet-show -c gateway_ip -f value $subnet_id
-}
-
-function wait_for_ping() {
-	ip=$1
-	ping -c 1 $ip 1>/dev/null 2>&1
-	wait=$?
-	while [ "$wait" -ne "0" ]
-	do
-  		echo $(date): Could not ping $ip yet...waiting...
-  		sleep 15
-		ping -c 1 $ip 1>/dev/null 2>&1
-  		wait=$?
-	done
-
-	echo
-	echo $(date): Ping $ip successful.
-	echo
-}
-
-function wait_for_ssl() {
-	ip=$1
-	echo | openssl s_client -connect $ip:443 >/dev/null 2>&1
-	wait=$?
-	while [ "$wait" -ne "0" ]
-	do
-  		echo $(date): Could not connect to HTTPS...waiting...
-  		sleep 15
-  		echo | openssl s_client -connect $ip:443 >/dev/null 2>&1
-  		wait=$?
-	done
-}
-
-function grid_ref() {
-	ip=$1
-	curl -sk -u admin:infoblox https://$ip/wapi/v2.3/grid | grep _ref | cut -d: -f2-3 | tr -d '," '
-}
-
-function gm_ref() {
-	ip=$1
-	curl -sk -u admin:infoblox https://$ip/wapi/v2.3/member?host_name=infoblox.localdomain | grep _ref | cut -d: -f2-3 | tr -d '," '
-}
-
-function wait_for_wapi() {
-	ip=$1
-	ref=""
-	while [[ -z "$ref" ]]; do
-		echo $(date): Waiting for WAPI...
-		ref=$(grid_ref $ip)
-	done
-
-	echo 
-	echo $(date): Done - grid $ref
-	echo
-}
-
-function download_cert() {
-	ip=$1
-	file=$2
-	echo $(date): Downloading certificate from $ip for use in member join...
-	echo
-	echo | openssl s_client -connect $ip:443 2>/dev/null | openssl x509 | sed -e 's/^/    /' > $file
-	echo $(date): Done
-}
-
-
-function grid_set_ha() {
-	fip=$1
-	ref=$2
-	vip=$3
-	gw=$4
-	n1lan=$5
-	n1ha=$6
-	n2lan=$7
-	n2ha=$8
-	echo "On $fip, setting $ref networking to (vip=$vip, gw=$gw, n1lan=$n1lan, n1ha=$n1ha, n2lan=$n2lan, n2ha=$n2ha)..."
-	echo $(curl -sk -u admin:infoblox -X PUT -H 'Content-Type: application/json' -d "{\"enable_ha\": true, \"router_id\": 200, \"vip_setting\": {\"address\": \"$vip\", \"gateway\": \"$gw\", \"subnet_mask\": \"255.255.255.0\" }, \"node_info\": [{\"lan_ha_port_setting\": { \"ha_ip_address\": \"$n1ha\", \"mgmt_lan\": \"$n1lan\"}}, {\"lan_ha_port_setting\": {\"ha_ip_address\": \"$n2ha\", \"mgmt_lan\": \"$n2lan\"}}]}" https://$fip/wapi/v2.3/$ref)
-	echo
-}
-
-function grid_join() {
-	vip=$1
-	fip=$2
-	
-	echo "Joining $fip to grid at $vip..."
-	ref=$(grid_ref $fip)
-	echo $(curl -sk -u admin:infoblox -X POST "https://$fip/wapi/v2.3/$ref?_function=join&master=$vip&shared_secret=test&grid_name=Infoblox")
-	echo
-}
-
-function grid_snmp() {
-	fip=$1
-	echo "Enabling SNMP..."
-	echo $(curl -sk -u admin:infoblox -X PUT -H "Content-Type: application/json" -d '{"snmp_setting": {"queries_enable": true, "queries_community_string": "public"}}' https://$fip/wapi/v2.3/$(grid_ref $fip))
-}
-
-
-function grid_dns() {
-	fip=$1
-	echo "Enabling DNS..."
-	echo $(curl -sk -u admin:infoblox -X PUT -H "Content-Type: application/json" -d '{"enable_dns": true}' https://$fip/wapi/v2.3/$(grid_ref $fip))
-}
-
-function grid_nsgroup() {
-	fip=$1
-	echo "Adding a default nsgroup..."
-	echo $(curl -sk -u admin:infoblox -X POST -H "Content-Type: application/json" -d '{"name": "default", "is_grid_default": true, "grid_primary": [{"name": "infoblox.localdomain"}]}' https://$fip/wapi/v2.3/nsgroup)
-}
+source ./grid-lib.sh
 
 # main
 
@@ -141,7 +25,9 @@ function grid_nsgroup() {
 #node_1
 #vip_port
 
-eval $(heat resource-list gm-ha  | cut -f 2,3 -d\| | tr -d ' ' | grep -v + | tr '|' '=')
+wait_for_stack $STACK
+
+eval $(heat resource-list $STACK  | cut -f 2,3 -d\| | tr -d ' ' | grep -v + | tr '|' '=')
 
 # Get the various IPs for each node
 VIP=$(port_first_fixed_ip $vip_port)
@@ -160,41 +46,18 @@ wait_for_ssl $N1_FIP
 wait_for_wapi $N1_FIP
 
 echo "Setting Networking Parameters for HA on Node 1..."
-GM_REF=$(gm_ref $N1_FIP)
-grid_set_ha $N1_FIP $GM_REF $VIP $GW $N1_LAN $N1_HA $N2_LAN $N2_HA
+grid_set_ha $N1_FIP $(gm_ref $N1_FIP) $VIP $GW $N1_LAN $N1_HA $N2_LAN $N2_HA
 
 wait_for_ping $VIP_FIP
 wait_for_ssl $VIP_FIP
 wait_for_wapi $VIP_FIP
-
-download_cert $VIP_FIP /tmp/gm-$VIP_FIP-cert.pm
 
 grid_join $VIP $N2_FIP
 
 grid_snmp $VIP_FIP
 grid_dns $VIP_FIP
 grid_nsgroup $VIP_FIP
-
-FIP_NET_ID=$(neutron floatingip-show -c floating_network_id -f value $node_1_floating_ip)
-FIP_NET=$(neutron net-show -c name -f value $FIP_NET_ID)
-
-cat > gm-$VIP_FIP-env.yaml <<EOF
-# Heat environment for launching autoscale against GM $VIP_FIP
-parameters:
-  gm_vip: $VIP
-  external_network: $FIP_NET
-  gm_cert: |
-EOF
-
-cat >> gm-$VIP_FIP-env.yaml < /tmp/gm-$VIP_FIP-cert.pem
-
-cat >> gm-$VIP_FIP-env.yaml <<EOF
-parameter_defaults:
-  wapi_url: https://$VIP_FIP/wapi/v2.3/
-  wapi_username: admin
-  wapi_password: infoblox
-  wapi_sslverify: false
-EOF
+write_env $VIP_FIP $VIP $vip_floating_ip
 
 echo
 echo HA GM is now configured and ready.
