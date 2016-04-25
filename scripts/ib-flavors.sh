28e2f295e9e4f231e8db700b2add5d9cdb324e34
John Belamaric <jbelamaric@infoblox.com>
2016-04-25 16:13:12 +0000

    * Fix 1410 memory

--- scripts/ib-flavors.sh
+++ scripts/ib-flavors.sh
@@ -9,7 +9,7 @@ nova flavor-create --is-public true vnios-100.55 auto 1024 55 1 --swap 0 --ephem
 nova flavor-create --is-public true vnios-810.55 auto 2048 55 2 --swap 0 --ephemeral 0
 nova flavor-create --is-public true vnios-820.55 auto 4096 55 2 --swap 0 --ephemeral 0
 nova flavor-create --is-public true vnios-CP-V800.160 auto 2048 160 2 --swap 0 --ephemeral 0
-nova flavor-create --is-public true vnios-1410.160 auto 15360 160 4 --swap 0 --ephemeral 0
+nova flavor-create --is-public true vnios-1410.160 auto 8192 160 4 --swap 0 --ephemeral 0
 nova flavor-create --is-public true vnios-1420.160 auto 15360 160 4 --swap 0 --ephemeral 0
 nova flavor-create --is-public true vnios-2210.160 auto 15360 160 4 --swap 0 --ephemeral 0
 nova flavor-create --is-public true vnios-2220.160 auto 15360 160 4 --swap 0 --ephemeral 0
